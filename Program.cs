d9a27e157f77bb3fb49c2f5a77554a82b2760d57
Emily Loggins <em.loggins7@gmail.com>
2019-07-10 16:07:55 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace planet_and_spaceships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>(){"Mercury", "Mars"};
+
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            List<string> lastPlanetsList = new List<string>(){"Uranus", "Neptune"};
+
+            planetList.AddRange(lastPlanetsList);
+
+            planetList.Insert(1, "Earth");
+            planetList.Insert(2, "Venus");
+
+            planetList.Add("Pluto");
+
+            List<string> rockyPlanets = new List<string>();
+
+            rockyPlanets = planetList.GetRange(0, 4);
+
+            planetList.Remove("Pluto");
+        }
+    }
+}
